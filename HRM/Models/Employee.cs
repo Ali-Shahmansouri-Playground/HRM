201965758e784b6b7758b260b346ecf5ce873bfb
Ali-Shahmansouri-Playground <sctshahali@gmail.com>
2023-10-15 15:10:55 +0330

    Implement Employee model

---
+++ HRM/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace HRM.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public float Salary { get; set; }
+    }
+}
