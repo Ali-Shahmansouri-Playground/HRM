15fe8aa5cff75df1d0a72dda6222783ff4bf5a1c
Ali-Shahmansouri-Playground <sctshahali@gmail.com>
2023-10-15 16:13:34 +0330

    Implement Bank

---
+++ HRM/Bank.cs
@@ -0,0 +1,54 @@
+﻿using HRM.Utils;
+
+namespace HRM
+{
+    public class Bank
+    {
+        private float budget;
+        private string bankDataFilePath;
+
+        public float Budget
+        {
+            get { return budget; }
+            private set { budget = value; }
+        }
+
+        public Bank(float initialBudget, string bankDataFilePath)
+        {
+            Budget = initialBudget;
+            this.bankDataFilePath = bankDataFilePath;
+
+            LoadData();
+        }
+
+        public void AddToBudget(float amount)
+        {
+            Budget += amount;
+
+            SaveData();
+        }
+
+        public void DeductFromBudget(float amount)
+        {
+            if (amount > Budget)
+                throw new InvalidOperationException("Insufficient budget.");
+
+            Budget -= amount;
+
+            SaveData();
+        }
+
+        private void SaveData()
+        {
+            DataStorage<Bank>.SaveData(bankDataFilePath, this);
+        }
+
+        private void LoadData()
+        {
+            if (DataStorage<Bank>.LoadData(bankDataFilePath).Count > 0)
+            {
+                Budget = DataStorage<Bank>.LoadData(bankDataFilePath).FirstOrDefault().Budget;
+            }
+        }
+    }
+}
