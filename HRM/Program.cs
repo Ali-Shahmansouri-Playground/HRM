4c4459447c6cba183ec2c20f1e3b0533e4e80999
Ali-Shahmansouri-Playground <sctshahali@gmail.com>
2023-10-15 16:35:36 +0330

    Implement Simple Usage

--- HRM/Program.cs
+++ HRM/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HRM;
+using HRM.Models;
+
+HRSystem hrSystem = new HRSystem("employees.json", "bank.json");
+
+// Add employees
+Employee john = new Employee { Name = "Mahmood", Position = "Manager", Salary = 5000 };
+Employee alice = new Employee { Name = "Jamal", Position = "Senior Developer", Salary = 3000 };
+hrSystem.AddEmployee(john);
+hrSystem.AddEmployee(alice);
+
+// Promote employee
+hrSystem.PromoteEmployee("Mahmood", "CEO", 2000);
+
+// Demote employee
+hrSystem.DemoteEmployee("Jamal", "Junior Developer", 1000);
+
+// Remove employee
+hrSystem.RemoveEmployee("Jamal");
